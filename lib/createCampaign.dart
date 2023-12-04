658855f808412e3e838f14a9443187e6a4d0169b
tderudder <thomas.derudder@etudiant.univ-rennes1.fr>
2023-12-04 11:08:43 +0100

    View depending on screen size. Scrollable form. Adding required field. Taxonomic group select.

--- lib/createCampaign.dart
+++ lib/createCampaign.dart
@@ -1,29 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:project_ofb/campaignList.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 void main() {
   runApp(const CreateCampaign());
 }
 
-class CreateCampaign extends StatefulWidget{
+class CreateCampaign extends StatefulWidget {
   const CreateCampaign({super.key});
 
   @override
   State<CreateCampaign> createState() => _CreateCampaignState();
-
 }
 
-class _CreateCampaignState extends State<CreateCampaign>{
-
+class _CreateCampaignState extends State<CreateCampaign> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final titreController = TextEditingController();
-  DateTime dateDebut = DateTime.now();
-  DateTime dateFin = DateTime.now();
+  DateTime dateDebut = DateTime(2000, 1, 1);
+  DateTime dateFin = DateTime(2000, 1, 1);
   final descriptionController = TextEditingController();
   final territoireController = TextEditingController();
   final groupesTaxonomiquesController = TextEditingController();
   final fichesController = TextEditingController();
 
+  final double minFieldWidth = 300.0;
+
+  static final List<String> _animals = [
+    "Lion",
+    "Flamingo",
+    "Hippo",
+    "Horse",
+    "Tiger",
+    "Penguin",
+    "Spider",
+    "Snake",
+    "Bear",
+    "Beaver",
+    "Cat",
+    "Fish",
+    "Hugo Hamon",
+    "Rabbit",
+    "Mouse",
+    "Dog",
+    "Zebra",
+    "Cow",
+    "Frog",
+    "Blue Jay",
+    "Moose",
+    "Gecko",
+    "Kangaroo",
+    "Shark",
+    "Crocodile",
+    "Owl",
+    "Dragonfly",
+    "Dolphin",
+  ];
+
+  final _items = _animals
+      .map((animal) => MultiSelectItem<String>(animal, animal.toString()))
+      .toList();
+
+  List<String?> _selectedAnimals = [];
 
   @override
   void dispose() {
@@ -39,74 +77,131 @@ class _CreateCampaignState extends State<CreateCampaign>{
 
   void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
     dateDebut = args.value.startDate;
-    if(args.value.endDate != null){
+    if (args.value.endDate != null) {
       dateFin = args.value.endDate;
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Création d'une nouvelle campagne"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                  hintText: "Titre"
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Align(
+            alignment: Alignment.center,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: (screenSize.width / 2 < minFieldWidth)
+                        ? minFieldWidth
+                        : screenSize.width / 2,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Titre",
+                      ),
+                      controller: titreController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez remplir ce champ';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: (screenSize.width / 2 < minFieldWidth)
+                        ? minFieldWidth
+                        : screenSize.width / 2,
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        hintText: "Description",
+                      ),
+                      controller: descriptionController,
+                    ),
+                  ),
+                  SizedBox(
+                    width: (screenSize.width / 2 < minFieldWidth)
+                        ? minFieldWidth
+                        : screenSize.width / 2,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Territoire",
+                      ),
+                      controller: territoireController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez remplir ce champ';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: (screenSize.width / 2 < minFieldWidth)
+                        ? minFieldWidth
+                        : screenSize.width / 2,
+                    child: MultiSelectDialogField(
+                      items: _animals
+                          .map((e) => MultiSelectItem(e, e.toString()))
+                          .toList(),
+                      listType: MultiSelectListType.CHIP,
+                      onConfirm: (values) {
+                        _selectedAnimals = values;
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: (screenSize.width / 4 < minFieldWidth)
+                        ? minFieldWidth
+                        : screenSize.width / 4,
+                    width: (screenSize.width / 4 < minFieldWidth)
+                        ? minFieldWidth
+                        : screenSize.width / 4,
+                    child: SfDateRangePicker(
+                      onSelectionChanged: _onSelectionChanged,
+                      selectionMode: DateRangePickerSelectionMode.range,
+                    ),
+                  ),
+                ],
               ),
-              controller: titreController,
             ),
-            TextField(
-              decoration: const InputDecoration(
-                  hintText: "Description"
-              ),
-              controller: descriptionController,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                  hintText: "Territoire"
-              ),
-              controller: territoireController,
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                  hintText: "Groupes Taxonomiques"
-              ),
-              controller: groupesTaxonomiquesController,
-            ),
-            Container(
-              // Adjust the height and width of the container to control the size of the date picker
-              height: 200, // Adjust the height as needed
-              width: 300, // Adjust the width as needed
-              child:
-                SfDateRangePicker(
-                  onSelectionChanged: _onSelectionChanged,
-                  selectionMode: DateRangePickerSelectionMode.range,
-                ),
-              ),
-
-          ],
+          ),
         ),
       ),
       floatingActionButton: Container(
         alignment: Alignment.bottomCenter,
         child: ElevatedButton(
           child: const Text('Lets go !'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const CampaignList()),
-              );
+          onPressed: () {
+            // Valider le formulaire avant de procéder
+            if (_formKey.currentState!.validate()) {
+              // Si le formulaire est valide, naviguer vers la prochaine page
+              if (dateFin != DateTime(2000, 1, 1)) {
+                // Si le formulaire est valide et une plage de dates a été sélectionnée,
+                // naviguer vers la prochaine page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const CampaignList()),
+                );
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Veuillez sélectionner une plage de dates.'),
+                  ),
+                );
+              }
             }
+          },
         ),
       ),
     );
   }
-
 }
-
