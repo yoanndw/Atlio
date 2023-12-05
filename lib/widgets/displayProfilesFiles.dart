52a1b73a30ae356fa1c98f88bfdb40f61b789441
Enora Danilo <enora.danilo@etudiant.univ-rennes1.fr>
2023-12-05 09:07:47 +0100

    Update displayProfilesFiles design

--- lib/widgets/displayProfilesFiles.dart
+++ lib/widgets/displayProfilesFiles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_ofb/homePage.dart';
 
 class DisplayFiles extends StatelessWidget {
   //final Fiche fiche;
@@ -13,48 +14,73 @@ class DisplayFiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.symmetric(vertical: 5),
-        decoration: BoxDecoration(
-            //color: Theme.of(context).colorScheme.secondaryContainer,
-            border: Border.symmetric(
-                horizontal: BorderSide(color: Colors.grey.shade200, width: 1),
-                vertical: BorderSide.none)),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image.asset(
-                  'assets/profil.png',
-                  height: 75,
-                  width: 75,
-                ),
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        textStyle: TextStyle(color: Colors.black87),
+        backgroundColor: Colors.white54,
+        // Couleur de fond
+        side: const BorderSide(
+          color: Colors.white,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0), // <-- Radius
+        ),
+      ),
+      onPressed: () {
+        // TODO changer la redirection
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const HomePage(title: 'hahahaha')),
+        );
+      },
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                'assets/profil.png',
+                height: 75,
+                width: 75,
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                DefaultTextStyle.merge(
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  child: Text('Nom de la campagne'),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DefaultTextStyle.merge(
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
                 ),
-                Row(
-                  children: [
-                    Padding(
-                        padding: const EdgeInsets.only(right: 10),
+                child: Text('Nom de la campagne'),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: DefaultTextStyle.merge(
+                        style: const TextStyle(
+                          color: Colors.black87,
+                        ),
                         child: Text("Lieu")),
-                    Text("Date et heure")
-                  ],
-                )
-              ],
-            )
-          ],
-        ));
+                  ),
+                  DefaultTextStyle.merge(
+                      style: const TextStyle(
+                        color: Colors.black87,
+                      ),
+                      child: Text("Date et heure"))
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
