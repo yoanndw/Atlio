c9be2d514edb1e942c58aaa653eca4d1495281a8
yoanndw <yoanndewilde@gmail.com>
2023-12-05 22:24:52 +0100

    Delete Firebase deps

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -4,13 +4,7 @@ import 'package:project_ofb/createCampaign.dart';
 import 'package:project_ofb/profile.dart';
 import 'package:project_ofb/map.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
