28ba5be460d2c29a5fb320268b6739901c3f8504
yoanndw <yoanndewilde@gmail.com>
2023-12-06 15:44:40 +0100

    Init Firebase in homePage.dart

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:project_ofb/profile.dart';
 import 'package:project_ofb/widgets/menu.dart';
+import 'package:project_ofb/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
