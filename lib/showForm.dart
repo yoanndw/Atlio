bc837de56d63d3893a6ca053d4a3724fe574d310
Enora Danilo <enora.danilo@etudiant.univ-rennes1.fr>
2023-12-06 14:18:47 +0100

    Update showForm.dart

--- lib/showForm.dart
+++ lib/showForm.dart
@@ -44,62 +44,60 @@ class _ShowFormState extends State<ShowForm> {
             ),
           ],
         ),
-        body: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                    height: 200,
-                    width: double.infinity,
-                    child: Image.asset(widget.fiche.photos.elementAt(0))),
-                Padding(
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      height: 200,
+                      width: double.infinity,
+                      child: Image.asset(widget.fiche.photos.elementAt(0))),
+                  Padding(
+                      padding: EdgeInsets.only(left: 15, right: 15, top: 10),
+                      child: Row(
+                        children: [
+                          Icon(Icons.room_outlined),
+                          Text(widget.fiche.lieu)
+                        ],
+                      )),
+                  Padding(
+                      padding: EdgeInsets.only(left: 15, right: 15, top: 10),
+                      child: Row(
+                        children: [
+                          Icon(Icons.access_time_outlined),
+                          Text(" " +
+                              widget.fiche.dateHeure.day.toString() +
+                              "/" +
+                              widget.fiche.dateHeure.month.toString() +
+                              "/" +
+                              widget.fiche.dateHeure.year.toString() +
+                              " - " +
+                              widget.fiche.dateHeure.hour.toString() +
+                              ":" +
+                              widget.fiche.dateHeure.minute.toString())
+                        ],
+                      )),
+                  Padding(
+                      padding: EdgeInsets.only(left: 15, right: 15, top: 10),
+                      child: Row(
+                        children: [
+                          Icon(Icons.person_outline),
+                          Text(widget.fiche.user)
+                        ],
+                      )),
+                  Padding(
                     padding: EdgeInsets.only(left: 15, right: 15, top: 10),
-                    child: Row(
-                      children: [
-                        Icon(Icons.room_outlined),
-                        Text(widget.fiche.lieu)
-                      ],
-                    )),
-                Padding(
-                    padding: EdgeInsets.only(left: 15, right: 15, top: 10),
-                    child: Row(
-                      children: [
-                        Icon(Icons.access_time_outlined),
-                        Text(" " +
-                            widget.fiche.dateHeure.day.toString() +
-                            "/" +
-                            widget.fiche.dateHeure.month.toString() +
-                            "/" +
-                            widget.fiche.dateHeure.year.toString() +
-                            " - " +
-                            widget.fiche.dateHeure.hour.toString() +
-                            ":" +
-                            widget.fiche.dateHeure.minute.toString())
-                      ],
-                    )),
-                Padding(
-                    padding: EdgeInsets.only(left: 15, right: 15, top: 10),
-                    child: Row(
-                      children: [
-                        Icon(Icons.person_outline),
-                        Text(widget.fiche.user)
-                      ],
-                    )),
-                Padding(
-                    padding: EdgeInsets.only(left: 15, right: 15, top: 10),
-                    child: Row(
-                      children: [
-                        //Icon(Icons.create_outlined),
-                        Flexible(
-                            child: Text(widget.fiche.observation,
-                                textAlign: TextAlign.justify))
-                      ],
-                    )),
-              ],
-            ),
-            Menu(isMenuOpen: _isMenuOpen),
-          ],
+                    child: Flexible(
+                        child: Text(widget.fiche.observation,
+                            textAlign: TextAlign.justify)),
+                  ),
+                ],
+              ),
+              Menu(isMenuOpen: _isMenuOpen),
+            ],
+          ),
         ));
   }
 }
