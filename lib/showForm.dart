cf822b2403be5d3f5a976deb7adf6bacfc9a1065
Samy Solhi <samy.solhi@etudiant.univ-rennes1.fr>
2023-12-08 11:04:45 +0100

    Added map to show form

--- lib/showForm.dart
+++ lib/showForm.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_ofb/widgets/menu.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart' as osm;
 
 import 'model/fiche.dart';
 
@@ -12,12 +13,25 @@ class ShowForm extends StatefulWidget {
   State<ShowForm> createState() => _ShowFormState();
 }
 
-class _ShowFormState extends State<ShowForm> {
+class _ShowFormState extends State<ShowForm> with osm.OSMMixinObserver{
   bool _isMenuOpen = false;
   String? _campaignName;
 
+  final double minFieldWidth = 300.0;
+
+  osm.MapController controller = osm.MapController(
+    initPosition: osm.GeoPoint(latitude: 47.083328, longitude: -1.68333),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addObserver(this);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -72,11 +86,63 @@ class _ShowFormState extends State<ShowForm> {
                         child: Text(widget.fiche.observation,
                             textAlign: TextAlign.justify)),
                   ),
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white70,
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(0.0),
+                      ),
+                      height: (screenSize.width / 2 < minFieldWidth)
+                          ? minFieldWidth
+                          : screenSize.width / 2,
+                      width: (screenSize.width / 2 < minFieldWidth)
+                          ? minFieldWidth
+                          : screenSize.width / 2,
+                      child: osm.OSMFlutter(
+                        controller: controller,
+                        osmOption: osm.OSMOption(
+                          userTrackingOption: const osm.UserTrackingOption(
+                            enableTracking: true,
+                            unFollowUser: false,
+                          ),
+                          zoomOption: const osm.ZoomOption(
+                            initZoom: 8,
+                            minZoomLevel: 3,
+                            maxZoomLevel: 19,
+                            stepZoom: 1.0,
+                          ),
+                          userLocationMarker: osm.UserLocationMaker(
+                            personMarker: const osm.MarkerIcon(
+                              icon: Icon(
+                                Icons.location_history_rounded,
+                                color: Colors.red,
+                                size: 48,
+                              ),
+                            ),
+                            directionArrowMarker: const osm.MarkerIcon(
+                              icon: Icon(
+                                Icons.double_arrow,
+                                size: 48,
+                              ),
+                            ),
+                          ),
+                          roadConfiguration: const osm.RoadOption(
+                            roadColor: Colors.yellowAccent,
+                          ),
+                        ),
+                      )),
                 ],
               ),
             ),
             Menu(isMenuOpen: _isMenuOpen),
           ],
         ));
+  }
+
+  @override
+  Future<void> mapIsReady(bool isReady) {
+    controller.goToLocation(osm.GeoPoint(latitude: widget.fiche.positionGps["lat"]!, longitude: widget.fiche.positionGps["lon"]!));
+    // controller.addMarker(osm.GeoPoint(latitude: widget.fiche.positionGps["lat"]!, longitude: widget.fiche.positionGps["lon"]!));
+    return Future(() => null);
   }
 }
